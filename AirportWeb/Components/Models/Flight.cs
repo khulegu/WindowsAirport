83280f92b5646090239c4bae9666c3731f37160e
Dulguun Narantuya <19b1num0011@stud.num.edu.mn>
2025-06-04 05:17:40 +0800

    Initialized Web

---
+++ AirportWeb/Components/Models/Flight.cs
@@ -0,0 +1,12 @@
+﻿namespace FlightStatusWeb.Components.Models
+{
+    public class Flight
+    {
+        public string FlightNumber { get; set; }
+        public string Destination { get; set; }
+        public DateTime Time { get; set; }
+        public string Gate { get; set; }
+        public string Status { get; set; }
+    }
+
+}
